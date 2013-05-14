c87f22c06ec75ffc623d6f815071c3ea3d0088cb
Jason Bentley <jbxfade@fullsail.edu>
2013-05-13 19:25:01 -0700

    proj2 base views

---
+++ Project2/Project2/ThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.m
+//  Project2
+//
+//  Created by Jason Bentley on 5/13/13.
+//  Copyright (c) 2013 Jason Bentley. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
