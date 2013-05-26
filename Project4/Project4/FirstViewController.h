cc1734d6e842577fdd504ee14cb5158a3e7aa998
Jason Bentley <jbxfade@fullsail.edu>
2013-05-25 18:51:04 -0700

    added proj 4 folder

---
+++ Project4/Project4/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Project4
+//
+//  Created by Jason Bentley on 5/25/13.
+//  Copyright (c) 2013 Jason Bentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Project4/Project4/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  Project4
+//
+//  Created by Jason Bentley on 5/25/13.
+//  Copyright (c) 2013 Jason Bentley. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
